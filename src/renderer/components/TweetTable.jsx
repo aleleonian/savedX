dacd5b9f13e4035068e9ddc9df94c8e32388d57e
Alejandro Leonian <66581753+aleleonian@users.noreply.github.com>
2024-04-22 09:15:15 -0500

    implemented filter and pagination

--- src/renderer/components/TweetTable.jsx
+++ src/renderer/components/TweetTable.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { CompactTable } from '@table-library/react-table-library/compact';
 import { useTheme } from "@table-library/react-table-library/theme";
-import { getTheme } from "@table-library/react-table-library/baseline";
 
 const columns = [
     { label: 'Tweet', renderCell: (item) => item.tweetText },
--- src/renderer/components/TweetTablePagination2.jsx
+++ src/renderer/components/TweetTablePagination2.jsx
@@ -6,59 +6,85 @@ import { getTheme } from "@table-library/react-table-library/baseline";
 import { usePagination } from "@table-library/react-table-library/pagination";
 
 
-const TweetTablePagination2 = ({nodes}) => {
-  const data = { nodes };
+const TweetTablePagination2 = ({ nodes }) => {
 
-  const theme = useTheme(getTheme());
+    const [search, setSearch] = React.useState("");
 
-  const pagination = usePagination(data, {
-    state: {
-      page: 0,
-      size: 2,
-    },
-    onChange: onPaginationChange,
-  });
+    let data = { nodes };
 
-  function onPaginationChange(action, state) {
-    console.log(action, state);
-  }
+    debugger;
 
-  const columns = [
-    { label: 'Tweet', renderCell: (item) => item.tweetText },
-];
-  return (
-    <>
-      <CompactTable
-        columns={columns}
-        data={data}
-        theme={theme}
-        pagination={pagination}
-      />
+    const handleSearch = (event) => {
+        setSearch(event.target.value);
+    };
 
-      <br />
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <span>Total Pages: {pagination.state.getTotalPages(data.nodes)}</span>
+    data = {
+        nodes: data.nodes.filter((item) =>
+            item.tweetText.toLowerCase().includes(search.toLowerCase())
+        ),
+    };
 
-        <span>
-          Page:{" "}
-          {pagination.state.getPages(data.nodes).map((_, index) => (
-            <button
-              key={index}
-              type="button"
-              style={{
-                fontWeight: pagination.state.page === index ? "bold" : "normal",
-              }}
-              onClick={() => pagination.fns.onSetPage(index)}
-            >
-              {index + 1}
-            </button>
-          ))}
-        </span>
-      </div>
 
-      <br />
-    </>
-  );
+    const theme = useTheme(getTheme());
+
+    const pagination = usePagination(data, {
+        state: {
+            page: 0,
+            size: 2,
+        },
+        onChange: onPaginationChange,
+    });
+
+    function onPaginationChange(action, state) {
+        console.log(action, state);
+    }
+
+    const columns = [
+        { label: 'Tweet', renderCell: (item) => item.tweetText },
+    ];
+    return (
+        <>
+            <CompactTable
+                columns={columns}
+                data={data}
+                theme={theme}
+                pagination={pagination}
+            />
+
+            <br />
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+
+                <div className="search-box p-[11px] text-left flex justify-between">
+                    <div>
+                        Search by Tweet Text:
+                    </div>
+                    <div className='bg-red'>
+                        <input id="search" type="text" value={search} onChange={handleSearch} className='border' />
+                    </div>
+                </div>
+
+                <span>Total Pages: {pagination.state.getTotalPages(data.nodes)}</span>
+
+                <span>
+                    Page:{" "}
+                    {pagination.state.getPages(data.nodes).map((_, index) => (
+                        <button
+                            key={index}
+                            type="button"
+                            style={{
+                                fontWeight: pagination.state.page === index ? "bold" : "normal",
+                            }}
+                            onClick={() => pagination.fns.onSetPage(index)}
+                        >
+                            {index + 1}
+                        </button>
+                    ))}
+                </span>
+            </div>
+
+            <br />
+        </>
+    );
 };
 
 export default TweetTablePagination2;
