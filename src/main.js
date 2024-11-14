385d4bcf45c341eba234fbf2fc86ec14043b62d6
Alejandro Leonian <alejandromleonian@gmail.com>
2024-11-14 18:55:22 -0500

    fixed the closing button thingy

--- src/main.js
+++ src/main.js
@@ -52,22 +52,21 @@ app.whenReady().then(async () => {
   if (!initStatus.success) {
     sendMessageToMainWindow("NOTIFICATION", `error--${initStatus.errorMessage}`);
   }
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+});
+
+// On OS X it's common to re-create a window in the app when the
+// dock icon is clicked and there are no other windows open.
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  app.quit();
 });
 
 app.on("before-quit", () => {
